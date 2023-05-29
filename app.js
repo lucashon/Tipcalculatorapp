f49c1f9b71fecf885c237f0c608daa1f9a526081
Lucas Honorio <110117686+lucashon@users.noreply.github.com>
2023-05-29 18:09:19 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+const apagar = document.getElementById('apagar') //Reset color
+
+
+//RESET COLOR  
+apagar.addEventListener('mouseover', ()=>{
+   apagar.style.backgroundColor = 'hsl(172, 77%, 56%)'
+})
+apagar.addEventListener('mouseout', ()=>{
+   apagar.style.backgroundColor = 'hsl(172, 67%, 45%)'
+})
